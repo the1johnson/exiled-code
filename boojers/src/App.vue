7c13f4de72fc1a48088ff750f2b1b689dec771c2
Toby <toby@booj.com>
2020-03-20 16:51:36 -0600

    formatting change

--- boojers/src/App.vue
+++ boojers/src/App.vue
@@ -1,22 +1,21 @@
 <template>
-	<div id="app">
-		<HeaderComponent />
-		<main>
-			<router-view />
-		</main>
-		<FooterComponent />
-	</div>
+  <div id="app">
+    <HeaderComponent />
+    <main>
+      <router-view />
+    </main>
+    <FooterComponent />
+  </div>
 </template>
 
 <script>
-
-import HeaderComponent from '@/components/header.vue'
-import FooterComponent from '@/components/footer.vue'
-export default {
-name: 'App',
-	components: {
-		HeaderComponent,
-		FooterComponent
-	}
-}
+  import HeaderComponent from '@/components/header.vue'
+  import FooterComponent from '@/components/footer.vue'
+  export default {
+    name: 'App',
+    components: {
+      HeaderComponent,
+      FooterComponent
+    }
+  }
 </script>
